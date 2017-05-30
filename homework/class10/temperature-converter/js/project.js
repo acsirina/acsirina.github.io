e70d8e310af8dc78dfa53ac00fd2c731ba4c4d4b
acsirina <irina.a@irinas-MBP.home>
2017-05-29 21:33:36 -0400

    Homework week#11

--- homework/class10/temperature-converter/js/project.js
+++ homework/class10/temperature-converter/js/project.js
@@ -1,11 +1,45 @@
-// document.getElementById('#convert').onclick = convert;
 
-// 	function convert(){
-// 	var celInput = document.getElementById('cel_input').value;
-// 	var farInput = document.getElementById('far_input').value;
-// 	var kelInput= document.getElementById('kel_input').value;
-	
-	
 
-// 	var result = (farInput - 32) / 1.8;
-// }
+
+$(document).ready(function(){
+
+$('#far-input').keydown(function(){
+		// console.log('keydown working');
+        var farInput  = parseInt($(this).val());
+        var celOutput = (farInput - 32) / 1.8;
+         $('#cel-input').val(celOutput);
+
+    })
+
+
+$('#cel-input').keydown(function(){
+		console.log('keydown working');
+        var celInput  = parseInt($(this).val());
+        var falOutput = celInput * (9 / 5) + 32;
+        
+
+        $('#far-input').val(falOutput);
+
+        if (falOutput > 80) {
+        $('body').css('background-color', 'red');
+    }
+    })
+})
+    
+
+
+// if(lights =='on'){
+//      $('#lightswitch').css('background-color', 'white');
+//      $('#lightswitch').css('color', 'black');
+//      $('body').css('background-color', 'black');
+    
+//      lights = 'off';
+//  } else {
+//      $('#lightswitch').css('background-color', 'black');
+//      $('#lightswitch').css('color', 'white');
+//      $('body').css('background-color', 'white');
+
+//      lights = 'on';
+//  }
+
+// })
